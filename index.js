187689bed45a2d42d2b1ba571976e27c484a4f65
mstanka <mice78.mm@gmail.com>
2021-04-09 20:57:45 +0200

    add game buttons

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const addButtons = () => {
+  for (let i = 0; i < 100; i++) {
+    const buttonEl = `
+      <button class="game__btn"></button>
+      `;
+    document.querySelector('.game').insertAdjacentHTML('afterbegin', buttonEl);
+  }  
+};
+
+window.addEventListener('load', addButtons);
