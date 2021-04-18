9ffe44cb58169ceeb47c1d8a496d495cd3ec9346
mstanka <mice78.mm@gmail.com>
2021-04-18 20:01:42 +0200

    draw circle or cross in the game field

--- js/index.js
+++ js/index.js
@@ -1,15 +1,53 @@
 'use strict';
 
-/// create 100x button element
-const gameEl = document.querySelector('.game');
+const gameBtns = document.querySelectorAll('.game__btn');
+const iconEl = document.querySelector('.icon');
 
-const addButtons = () => {
-  for (let i = 0; i < 100; i++) {
-    const buttonEl = `
-      <button class="game__btn"></button>
-      `;
-    gameEl.insertAdjacentHTML('afterbegin', buttonEl);
+let player = 'circle';
+
+// change player
+const changePlayer = () => {
+  if (player === 'circle') {
+    player = 'cross';
+    iconEl.setAttribute('src', './img/cross.svg');
+  } else {
+    player = 'circle';
+    iconEl.setAttribute('src', './img/circle.svg');
   }
 };
 
-window.addEventListener('load', addButtons);
+// draw circle or cross in the button field
+const fillBtn = (i, item) => {
+  gameBtns[i].classList.add(`game__btn--${item}`);
+  gameBtns[i].setAttribute('disabled', true);
+};
+
+// loop the NodeList of buttons and listen for click
+for (let i = 0; i < gameBtns.length; i++) {
+  gameBtns[i].addEventListener('click', () => {
+    if (player === 'circle') {
+      fillBtn(i, 'circle');
+    }
+    if (player === 'cross') {
+      fillBtn(i, 'cross');
+    }
+    changePlayer();
+  });
+}
+
+/////////////////////////////
+/// create 100x button element
+/// when I use 'document.querySelectorAll('.game__btn')' I get NodeList length = 0, and cannot target the button
+
+// const gameEl = document.querySelector('.game');
+
+// const addButtons = () => {
+//   for (let i = 0; i < 100; i++) {
+//     const buttonEl = `
+//       <button class="game__btn"></button>
+//       `;
+//     gameEl.insertAdjacentHTML('afterbegin', buttonEl);
+//   }
+// };
+
+// window.addEventListener('load', addButtons);
